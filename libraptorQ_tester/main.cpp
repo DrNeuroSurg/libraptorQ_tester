4f2ffe61623f0c96cd0219cace50ebe618ebf9c3
Andras Oravecz <oraveczandrew@gmail.com>
2016-04-30 12:50:41 +0200

    Initial Commit

---
+++ libraptorQ_tester/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  libraptorQ_tester
+//
+//  Created by András Oravecz on 30/04/16.
+//  Copyright © 2016 András Oravecz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
